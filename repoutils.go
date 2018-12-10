9fe72d02e29e67feaa842c9581e5e19cf698ecc0
Matt Meyers <matt.meyers@mcgstrategic.com>
2018-12-10 16:09:04 -0500

    Added json repo configuration file and accompanying utils.

---
+++ repoutils.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+type LocalRepo struct {
+	Name     string `json:"name"`
+	FullName string `json:"fullName"`
+	Branch   string `json:"branch"`
+	Path     string `json:"path"`
+}
+
+func GetAllLocalRepos() map[string]LocalRepo {
+	return readInFile()
+}
+
+func GetLocalRepo(name string) LocalRepo {
+	repos := readInFile()
+	return repos[name]
+}
+
+func AddLocalRepo(repo LocalRepo) {
+	repos := readInFile()
+	repos[repo.FullName] = repo
+	writeToFile(repos)
+}
+
+func DeleteLocalRepo(repo string) {
+	repos := readInFile()
+	delete(repos, repo)
+	writeToFile(repos)
+}
+
+func readInFile() map[string]LocalRepo {
+	var repos map[string]LocalRepo
+	configFile, err := os.Open("repos.json")
+	if err != nil {
+		panic(fmt.Sprintf("Failed to open repos.json\n%s", err))
+	}
+	defer configFile.Close()
+
+	if err := json.NewDecoder(configFile).Decode(&repos); err != nil {
+		panic(fmt.Sprintf("Failed to parse json\n%s", err))
+	}
+
+	return repos
+}
+
+func writeToFile(repos map[string]LocalRepo) {
+	reposJson, err := json.Marshal(repos)
+	if err != nil {
+		panic(err)
+	}
+	if err = ioutil.WriteFile("repos.json", reposJson, 0644); err != nil {
+		panic(err)
+	}
+
+}
