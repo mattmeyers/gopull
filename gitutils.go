e3bdb894c4995966768637e197a45de587ebea16
Matt Meyers <matt.meyers@mcgstrategic.com>
2018-12-17 16:34:34 -0500

    Updated git command error message.

--- gitutils.go
+++ gitutils.go
@@ -10,7 +10,7 @@ func GitPull(repo LocalRepo) {
 	cmd := exec.Command(fmt.Sprintf("./deployment_scripts/%s", repo.DeploymentScript), repo.Path)
 	out, err := cmd.CombinedOutput()
 	if err != nil {
-		log.Fatalf("cmd.Run() failed with %s\n", err)
+		log.Fatalf("cmd.CombinedOutput() failed with %s\n", err)
 	}
 	fmt.Printf("%s\n", string(out))
 }
